ed6a5ab31726d20e79a681c90697ef96dc151ac5
Kimberly Nguyen <nguyenkr@hotmail.com>
2023-07-22 04:20:38 -0700

    added payment and product componet

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule
