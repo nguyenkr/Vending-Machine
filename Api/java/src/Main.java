26fe1cf0629b5b44d067b0dfaed7417b01d40adf
Kimberly Nguyen <nguyenkr@hotmail.com>
2023-07-22 04:11:52 -0700

    update main and added angular starter

--- Api/java/src/Main.java
+++ Api/java/src/Main.java
@@ -1,8 +1,27 @@
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Database db = new Database();
+        HashMap<String, Product> productionDb = db.getProductDb();
+        setUpProductDB(productionDb);
+
+        for(String productName: productionDb.keySet()){
+            Product product = productionDb.get(productName);
+            System.out.println(product.getName() + "|   Inventory: " + product.getInventory() + "     Cost:" + product.getCost());
+        }
+
+    }
+
+    public static void setUpProductDB(HashMap<String, Product> productDb){
+        Product soda = new Product("soda", 5, .95);
+        Product candyBar = new Product("candyBar", 5, .60);
+        Product chips = new Product("chips", 5, .99);
+
+        productDb.put("soda", soda);
+        productDb.put("candyBar", candyBar);
+        productDb.put("chips", chips);
     }
 
 }
